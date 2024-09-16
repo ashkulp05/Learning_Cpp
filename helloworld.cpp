ba2ad1d2e0a70c6064850fef2467201d588f5f25
ashkulp05 <ashkulp@gmail.com>
2024-09-15 20:21:12 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+//THIS IS NOT MY CODE IT IS FROM https://www.geeksforgeeks.org/writing-first-c-program-hello-world-example/
+//Geeks for Geeks has a great breakdown of what all the parts of the program do!
+
+// C++ program to display "Hello World"
+ 
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+ 
+// Main() function: where the execution of
+// program begins
+int main()
+{
+    // Prints hello world
+    cout << "Hello World!";
+ 
+    return 0;
+}
